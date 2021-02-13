b5829b9c8c5d75343bbdbc3164a247efa49a648e
Mahendra Dube <59559474+dubemahendra@users.noreply.github.com>
2021-02-13 07:52:26 +0530

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+public class Main {
+    public static void main(String[] args){
+        int studentAge=42;
+        double studentGpa=3.45;
+
+        String  studentFirstName="Mahendra";
+        String  studentSecondtName="Dube";
+        char studentFirstInitial=studentFirstName.charAt(0);
+        char studentLastInitial=studentSecondtName.charAt(0);
+
+        boolean hasPerfectAttendance=true;
+
+        System.out.println(studentAge);
+        System.out.println(studentGpa);
+
+        System.out.println(hasPerfectAttendance);
+        System.out.println(studentFirstName);
+        System.out.println(studentSecondtName);
+
+        System.out.println(studentFirstInitial);
+        System.out.println(studentLastInitial);
+
+
+    }
+}
+
+
